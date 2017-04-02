8a0a6c7ace46afd70384fa0babd7915370d822d1
kogezakanag <kogezakanag@gmail.com>
2017-04-02 15:29:51 +0900

    set the path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   def configure_devise_parameter_sanitizer
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
   end
+  
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
   def after_sign_out_path_for(resource)
     root_path
