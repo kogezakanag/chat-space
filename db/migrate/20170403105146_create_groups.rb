57e7a51e658091eda462b932a29e01c049714540
kogezakanag <kogezakanag@gmail.com>
2017-04-03 21:10:27 +0900

    Add index: true in migration

--- db/migrate/20170403105146_create_groups.rb
+++ db/migrate/20170403105146_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.text :name, null: :false
-      t.reference :user, foreign_key: true
+      t.reference :user, foreign_key: true, index: true
       t.timestamps null: :false
     end
   end
