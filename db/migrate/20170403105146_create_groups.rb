5f993b0bcba7fc6372bfb527cd4fc8d4f1d4fdb7
kogezakanag <kogezakanag@gmail.com>
2017-04-03 20:55:53 +0900

    Edit reference in migration file

--- db/migrate/20170403105146_create_groups.rb
+++ db/migrate/20170403105146_create_groups.rb
@@ -2,9 +2,8 @@ class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.text :name, null: :false
-      t.reference :member_id, foreign_key: true
+      t.reference :user, foreign_key: true
       t.timestamps null: :false
     end
-    add_index :groups, :member_id
   end
 end
