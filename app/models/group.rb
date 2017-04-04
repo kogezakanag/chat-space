b2b75e90e6f12f10352c772b11e6c6eb95c23723
kogezakanag <kogezakanag@gmail.com>
2017-04-04 11:50:30 +0900

    Set up asociation user, group, groups-users, message

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  belongs_to :groups_users
+  has_many :messages
 end
--- app/models/groups_user.rb
+++ app/models/groups_user.rb
@@ -1,2 +1,4 @@
 class GroupsUser < ApplicationRecord
+  has_many :users
+  has_many :groups
 end
