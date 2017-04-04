e6baef10979310222b259c5d8cae36ae5146523d
kogezakanag <kogezakanag@gmail.com>
2017-04-04 11:16:37 +0900

    Create groups table

--- db/migrate/20170403105047_delete_odd_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteOddTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :groups
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403105047) do
+ActiveRecord::Schema.define(version: 20170403105146) do
+
+  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "name",       limit: 65535
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -30,4 +38,5 @@ ActiveRecord::Schema.define(version: 20170403105047) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "groups", "users"
 end
